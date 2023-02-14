efcccf9005aca622ccff8caf24e236d0bf077146
Luiz Kill <luizkill@gmail.com>
2023-02-13 21:38:22 -0300

    adds blur to badges with no url

--- src/index.js
+++ src/index.js
@@ -1,60 +1,65 @@
-import React, { useState, useLayoutEffect } from 'react';
+import React, { useState, useLayoutEffect } from "react";
 
 const HEIGHT_RATIO = 3.375;
 
-const getImage = (locale = 'en-us') => {
+const getImage = (locale = "en-us") => {
   return {
     ios: `https://linkmaker.itunes.apple.com/images/badges/${locale}/badge_appstore-lrg.svg`,
-    android: `https://bitte-tecnologia.github.io/google-play-badge-svg/img/${locale}_get.svg?sanitize=true`
-  }
-}
+    android: `https://bitte-tecnologia.github.io/google-play-badge-svg/img/${locale}_get.svg?sanitize=true`,
+  };
+};
 
 const ReactStoreBadge = ({
   platform,
   url,
-  locale = (typeof navigator !== 'undefined' && navigator.language || defaultLocale),
-  defaultLocale = 'en-us',
+  locale = (typeof navigator !== "undefined" && navigator.language) ||
+    defaultLocale,
+  defaultLocale = "en-us",
   width = 135,
   height = width / HEIGHT_RATIO,
-  target = "_blank"
+  newWindow = true,
 }) => {
-  locale = locale.toLowerCase()
+  locale = locale.toLowerCase();
+  const isUrlSet = url.length > 0;
 
   // handles ptBR
-  const nonHyphenatedRegex = /^[a-zA-Z]{4}$/
-  if(locale.match(nonHyphenatedRegex)) {
-    locale = `${locale.slice(0,2)}-${locale.slice(2)}`
+  const nonHyphenatedRegex = /^[a-zA-Z]{4}$/;
+  if (locale.match(nonHyphenatedRegex)) {
+    locale = `${locale.slice(0, 2)}-${locale.slice(2)}`;
   }
 
-  const [image, setImage] = useState(getImage(locale))
+  const [image, setImage] = useState(getImage(locale));
 
   const setDefaultImage = () => {
-    setImage(getImage(defaultLocale))
-  }
+    setImage(getImage(defaultLocale));
+  };
 
   useLayoutEffect(() => {
-    setImage(getImage(locale))
-  }, [locale])
+    setImage(getImage(locale));
+  }, [locale]);
 
   return (
     <a
       style={{
-        display: 'inline-block',
+        display: "inline-block",
         height: height,
         width: width,
+        cursor: isUrlSet ? "pointer" : "default",
       }}
       href={url}
-      target={target}
+      target={newWindow ? "_blank" : "_self"}
+      onClick={(event) => !isUrlSet && event.preventDefault()}
     >
       <img
         src={image[platform]}
         style={{
-          width: '100%',
-          height: '100%'
+          width: "100%",
+          height: "100%",
+          filter: isUrlSet ? "" : "blur(2px)",
         }}
         onError={setDefaultImage}
       />
     </a>
   );
-}
-export default ReactStoreBadge
+};
+export default ReactStoreBadge;
